c10a6e721de7a74529aabdabf8f3a71f155bedcf
ismael-dell <ismaeljaycarpio1992@gmail.com>
2017-04-09 13:16:40 +0800

    init 1

---
+++ Memberships/Entities/ItemType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("ItemType")]
+    public class ItemType
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemTypeId { get; set; }
+
+        [MaxLength(255)]
+        [Required(ErrorMessage = "Item Type Title is required")]
+        public string ItemTypeTitle { get; set; }
+    }
+}
