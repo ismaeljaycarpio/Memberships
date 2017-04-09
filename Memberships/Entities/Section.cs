57d0340c44ed3786f4a883e31ccf8a5b517b5c4b
ismael-dell <ismaeljaycarpio1992@gmail.com>
2017-04-09 12:10:40 +0800

    init commit

---
+++ Memberships/Entities/Section.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("Section")]
+    public class Section
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SectionId { get; set; }
+
+        [MaxLength(255)]
+        [Required(ErrorMessage="Section Title is required")]
+        public string Title { get; set; }
+
+    }
+}
