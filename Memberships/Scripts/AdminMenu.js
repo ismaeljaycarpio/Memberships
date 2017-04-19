85ec15cba8a76594c342e45d2fc12c13d1eb6f3e
ismael-dell <ismaeljaycarpio1992@gmail.com>
2017-04-19 11:21:45 +0800

    added hover functionality and the menu

---
+++ Memberships/Scripts/AdminMenu.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    //code goes here...
+    $('[data-admin-menu]').hover(function () {
+        //code goes here....
+        $('[data-admin-menu]').toggleClass('open');
+    });
+});
